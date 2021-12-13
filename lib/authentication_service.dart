4b2f7b517cecc70c56728f6bb6aed95d97b7fca8
lindsimon <lindsimon97@gmail.com>
2021-12-13 12:26:10 +0100

    kopplat användare till firebase
    Co-authored-by: Joakim <multihaxxor@users.noreply.github.com>
    Co-authored-by: gusfrenal <gusfrenal@users.noreply.github.com>
    Co-authored-by: danielcuracica <danielcuracica@users.noreply.github.com>

---
+++ lib/authentication_service.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthenticationService {
+  final FirebaseAuth firebaseAuth;
+
+  AuthenticationService(this.firebaseAuth);
+
+  Stream<User?> get authStateChanges => firebaseAuth.idTokenChanges();
+
+  Future<String?> signIn(
+      {required String email, required String password}) async {
+    try {
+      await firebaseAuth.signInWithEmailAndPassword(
+          email: email, password: password);
+      return "Signed In";
+    } on FirebaseAuthException catch (e) {
+      return e.message;
+    }
+  }
+
+  Future<String?> signUp(
+      {required String email, required String password}) async {
+    try {
+      await firebaseAuth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      return "Signed Up";
+    } on FirebaseAuthException catch (e) {
+      return e.message;
+    }
+  }
+}
