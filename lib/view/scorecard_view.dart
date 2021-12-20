aae6d12a23579a22cc4e56c59e4a7bda34598469
multihaxxor <samcrolol@gmail.com>
2021-12-20 10:55:55 +0100

    FireBase
    Co-authored-by: gusbacte <teoback@users.noreply.github.com>
    Co-authored-by: gusfrenal <gusfrenal@users.noreply.github.com>
    Co-authored-by: lindsimon <lindsimon@users.noreply.github.com>
    Co-authored-by: danielcuracica <danielcuracica@users.noreply.github.com>

--- lib/view/scorecard_view.dart
+++ lib/view/scorecard_view.dart
@@ -1,7 +1,12 @@
+// import 'dart:html';
+// import 'package:http/http.dart';
+// import 'dart:convert';
+// import 'dart:io';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:provider/provider.dart';
 
 class ScoreCard extends StatefulWidget {
   const ScoreCard({Key? key}) : super(key: key);
@@ -13,30 +18,164 @@ class ScoreCard extends StatefulWidget {
 final TextEditingController playerController = TextEditingController();
 
 class _ScoreCardState extends State<ScoreCard> {
+  final Stream<QuerySnapshot> scorecardTest =
+      FirebaseFirestore.instance.collection('ScorecardTest').snapshots();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: StreamBuilder(
-            stream:
-                FirebaseFirestore.instance.collection('scorecard1').snapshots(),
-            builder:
-                (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-              if (!snapshot.hasData) {
-                return Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('players'),
+            Container(
+              height: 150,
+              padding: const EdgeInsets.symmetric(vertical: 20),
+              child: StreamBuilder<QuerySnapshot>(
+                stream: scorecardTest,
+                builder: (
+                  BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot,
+                ) {
+                  if (snapshot.hasError) {
+                    return Text('this isnt workin');
+                  }
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Text('Loading');
+                  }
+                  final data = snapshot.requireData;
 
+                  return ListView.builder(
+                    itemCount: data.size,
+                    itemBuilder: (context, index) {
+                      return Text('${data.docs[index]['players']}');
+                    },
+                  );
+                },
+              ),
+            ),
+            Text(
+              'Wtire data to clod',
+              style: TextStyle(fontSize: 20),
+            ),
+            MyCustomForm()
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyCustomForm extends StatefulWidget {
+  @override
+  MyCustomFormState createState() {
+    return MyCustomFormState();
+  }
+}
+
+class MyCustomFormState extends State<MyCustomForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  var name = '';
+  var score = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    CollectionReference scorecardtest =
+        FirebaseFirestore.instance.collection('ScorecardTest');
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextFormField(
+              decoration: const InputDecoration(
+                icon: Icon(Icons.person),
+                hintText: 'name pls',
+                labelText: 'name plpx',
+              ),
+              onChanged: (value) {
+                name = value;
+              },
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'name ffs';
+                }
+                return null;
+              }),
+          TextFormField(
+            decoration: const InputDecoration(
+              icon: Icon(Icons.person),
+              hintText: 'score loser',
+              labelText: 'score loooser',
+            ),
+            onChanged: (value) {
+              score = int.parse(value);
+            },
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'enter score pls';
+              }
+              return null;
+            },
+          ),
+          SizedBox(height: 15),
+          Center(
+            child: ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('uploading score to firebase'),
+                    ),
+                  );
+
+                  scorecardtest
+                      .add({'name': name, 'score': score})
+                      .then((value) => print('User Added'))
+                      .catchError((error) => print('failed $error'));
+                }
+              },
+              child: Text('Submit'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+      /*
               return ListView(
                 children: snapshot.data!.docs.map((document) {
-                  return Container(
-                    child:
-                        Center(child: Text(document['namn']['ID'].toString())),
+                  return Center(
+                    child: ListTile(
+                        title: Text(
+                          "ID: " + document['ID'].toString(),
+                          style: const TextStyle(
+                              color: Colors.blueGrey,
+                              fontWeight: FontWeight.w900,
+                              fontSize: 15),
+                        ),
+                        subtitle: Text("Namn: " + document['namn'],
+                            style: const TextStyle(
+                                color: Colors.blueGrey,
+                                fontWeight: FontWeight.w900,
+                                fontSize: 15))),
                   );
                 }).toList(),
+              );
+            }),
+      ),
+    );
+  }
+}
+*/
 
-                /*  ElevatedButton(
+/* ElevatedButton(
                     // child: Icon(Icons.add),
                     onPressed: () {
                       FirebaseFirestore.instance
@@ -45,23 +184,6 @@ class _ScoreCardState extends State<ScoreCard> {
                     },
                     child: Text('Sign Up'),
                   ),
-                */
-              );
-            }),
-      ),
-    );
 
-    /* body: StreamBuilder(
-          stream;
-              FirebaseFirestore.instance.collection('scorecard1').snapshots(),
-          builder: (context, snapshot) {
-            if (!snapshot.hasData) return Text('Loading data, hurry up');
-            return Column(
-              children: <Widget>[
-                Text(snapshot.data.documents[0]['namn']),
-                Text(snapshot.data.documents[0]['ID'].toString())
-              ],
-            );
-          }), */
-  }
-}
+                  */
+    
